987d89ec2c94a67e5544c074e8d9722eb6d093b3
Phillia <37705707+psteine3@users.noreply.github.com>
2019-04-09 16:44:21 -0700

    Finished majority of player registry section, need to build a json or XML parser to deploy a config file

--- In Development/Asteroid_Spawner_Mod/Data/Scripts/Asteroid_Territory_Spawner/Player_Registrar.cs
+++ In Development/Asteroid_Spawner_Mod/Data/Scripts/Asteroid_Territory_Spawner/Player_Registrar.cs
@@ -20,11 +20,46 @@ namespace Asteroid_Territory_Spawner {
     class Player_Registrar {
         public List<MyPlayerInfo> PlayerInfos;
         public Player_Registrar() {
+            this.PlayerInfos = new List<MyPlayerInfo>();
+        }
 
+        public void Init() {
+            this.RegisterPlayers();
         }
 
         public void UpdateDistanceTraveled() {
+            foreach(MyPlayerInfo playerInfo in this.PlayerInfos) {
+                playerInfo.UpdatePosition();
+            }
+        }
 
+        public void RegisterPlayers() {
+            List<MyPlayerInfo> NewPlayerInfos = new List<MyPlayerInfo>();
+
+            List<IMyPlayer> players = new List<IMyPlayer>();
+            MyAPIGateway.Players.GetPlayers(players);
+
+            for (int i = 0; i < players.Count; i++) {
+                // Don't register non-human characters
+                if (players[i].IsBot || players[i].Character == null) {continue;}
+                if (players[i].Character.IsDead) {continue;}
+
+                bool isRegistered = false;
+                foreach(MyPlayerInfo playerInfo in this.PlayerInfos) {
+                    if (playerInfo.Player.IdentityId == players[i].IdentityId) {
+                        // Player already is registered
+                        isRegistered = true;
+                        NewPlayerInfos.Add(playerInfo);
+                        break;
+                    }
+                }
+                // We need to add this player to the registry
+                if (!isRegistered) {
+                    NewPlayerInfos.Add(new MyPlayerInfo(players[i]));
+                }
+            }
+
+            this.PlayerInfos = NewPlayerInfos;
         }
     }
 }
